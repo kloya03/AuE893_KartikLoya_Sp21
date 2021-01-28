d4d64e3e6d7b3f105e4238785540405308050573
kloya03 <kloya@g.clemson.edu>
2021-01-28 14:36:08 -0500

    ..

--- catkin_ws/build/catkin_generated/order_packages.py
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/administrator/git_ws/AuE893_KartikLoya_Sp21/catkin_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/administrator/git_ws/AuE893_KartikLoya_Sp21/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/administrator/git_ws/AuE893_KartikLoya_Sp21/catkin_ws/devel;/opt/ros/noetic' != '' else []
