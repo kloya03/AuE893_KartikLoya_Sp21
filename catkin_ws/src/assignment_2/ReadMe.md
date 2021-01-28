990bf5e54fc7e3037e1c7ec5ce03b04725861395
kloya03 <kloya@g.clemson.edu>
2021-01-28 11:16:10 -0500

    Assignmen_2 completed

---
+++ catkin_ws/src/assignment_2/ReadMe.md
@@ -0,0 +1,15 @@
+Assignment 2
+
+Learning outcomes:
+1. Working with Git.
+2. Creating a ROS workspace
+3. Writing a python script for operating the turtle in turtlesim
+
+
+In assignment_2 you will write three python codes 
+
+1. circle.py: Make the Turtle move in a circle with constant twist velocity2. 
+
+2. square_openloop.py: Make the Turtle move in a square of 2x2 units with 0.2 linear velocityand 0.2 rad/s angular velocity.
+
+3. square_closedloop.py: Make the Turtle move in a square of 3x3 with velocity control.Predefinethe coordinate points of the square in the program. Make the turtle move from (5,5) ->(8,5) ->(8,8) -> (5,8) -> (5,5)
---
+++ catkin_ws/src/assignment_2/src/scripts/circle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+
+def move_circle():
+    rospy.init_node('turtlebot_controller', anonymous=True)
+
+    # Create a publisher which can "talk" to Turtlesim and tell it to move
+    pub = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=1)
+     
+    # Create a Twist message and add linear x and angular z values
+    move_cmd = Twist()
+    move_cmd.linear.x = 1
+    move_cmd.angular.z = 1
+
+    # Save current time and set publish rate at 10 Hz
+    now = rospy.Time.now()
+    rate = rospy.Rate(10)
+
+    # For the next 10 seconds publish cmd_vel move commands to Turtlesim
+    while rospy.Time.now() < now + rospy.Duration.from_sec(10):
+        pub.publish(move_cmd)
+        rate.sleep()
+    
+
+
+if __name__ == '__main__':
+    try:
+        move_circle()
+    except rospy.ROSInterruptException:
+        pass
